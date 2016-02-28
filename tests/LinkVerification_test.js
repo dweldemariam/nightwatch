99d09dc70288725fa2ed8598a1eef5d8d91e53f0
Daniel Weldemariam <dweldemariam@dweldemariam.local>
2016-02-28 12:59:21 -0800

    Adds account and student tests

---
+++ tests/LinkVerification_test.js
@@ -0,0 +1,62 @@
+// module.exports = {
+//
+//     "Step one. About us" : function (browser) {
+//        browser
+//        .url("https://superqa.herokuapp.com/")
+//        .waitForElementVisible ('body', 1000)
+//        .click('a[class="recruter aboutus-tab"], About us')
+//        .pause (1000)
+//        .assert.urlEquals('https://superqa.herokuapp.com/aboutus')
+//
+//      },
+//
+//      "Step two. Courses" : function (browser) {
+//         browser
+//         .url("https://superqa.herokuapp.com/")
+//         .waitForElementVisible ('body', 1000)
+//         .click('a[class="recruter courses-tab"], About us')
+//         .pause (1000)
+//         .assert.urlEquals('https://superqa.herokuapp.com/courses')
+//
+//       },
+//
+//       "Step three. Staff" : function (browser) {
+//          browser
+//          .url("https://superqa.herokuapp.com/")
+//          .waitForElementVisible ('body', 1000)
+//          .click('a[class="recruter staff-tab"], About us')
+//          .pause (1000)
+//          .assert.urlEquals('https://superqa.herokuapp.com/staff')
+//
+//        },
+//
+//         "Step four. Login" : function (browser) {
+//           browser
+//           .url("https://superqa.herokuapp.com/")
+//           .waitForElementVisible ('body', 1000)
+//           .click('a[class="recruter login"]')
+//           .pause (1000)
+//           .waitForElementVisible ('div[class="loginForm"]', 500)
+//           .assert.elementPresent("form")
+//           .assert.elementPresent(".login-field")
+//           .assert.elementPresent('input[name=email]')
+//           .assert.elementPresent('input[name=password]')
+//           .assert.elementPresent(".button")
+//           .assert.containsText(".loginForm", "Forget password")
+//           .click('a[class="recruter login"]')
+//
+//         },
+//
+//           'Step five. Register' : function (browser) {
+//            browser
+//            .url("https://superqa.herokuapp.com/")
+//            .waitForElementVisible ('body', 1000)
+//            .click('a[class="recruter register"]')
+//            .pause (1000)
+//            .waitForElementVisible ('div[class="registrationForm"]', 500)
+//            .assert.elementPresent("form")
+//            .click('a[class="recruter register"]')
+//            .end();
+//         }
+//
+// };
